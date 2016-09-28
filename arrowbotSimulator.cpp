b4669f19134ed6b03f05884d494e8dd379f8b9ad
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2016-09-28 17:29:04 -0400

    Numeric integration done, no debug
    	modified:   arrowbotSimulator.cpp
    	modified:   arrowbotSimulator.h
    	modified:   evclib

--- arrowbotSimulator.cpp
+++ arrowbotSimulator.cpp
@@ -1,11 +1,19 @@
+#include <tuple>
 #include <iostream>
 #include <cstdlib>
 
 #include <boost/numeric/ublas/io.hpp>
+#include <boost/numeric/odeint.hpp>
 
 #include "arrowbotSimulator.h"
 
 /* Auxiliary functions */
+
+void exitWithError(const std::string& mess)
+{
+	std::cerr << mess << std::endl;
+	exit(EXIT_FAILURE);
+}
 
 void lowerTriangularOnes(matrix<double>& K, int size)
 {
@@ -19,12 +27,44 @@ void lowerTriangularOnes(matrix<double>& K, int size)
 
 // Private
 
-double ArrowbotSimulator::evaluateControllerForOrientations(int orientationsIdx)
+void ArrowbotSimulator::validateArrowbotParameters()
 {
-	auto itOrientation = parameters.begin() + orientationIdx;
+	if(segments() < 1)
+		exitWithError("Bad parameters: Arrowbot must have at least one segment. Exiting");
+	if(botParameters.sensorAttachment.size1() != segments() ||
+	   botParameters.sensorAttachment.size2() != segments())
+		exitWithError("Bad parameters: some size of the sensor attachment table does not match the number of segments. Exiting");
+	}
+	int maxSensorsPerSegment = 0;
+	int sensorsPerSegment = 0;
+	for(int i=0; i<segments(); i++)
+	{
+		for(int j=0; j<segments(); j++)
+		{
+			if(botParameters.sensorAttachment != 0.)
+			{
+				if(botParameters.sensorAttachment != 1.)
+					exitWithError("Bad parameters: attachment matrix must only contain zeros and ones. Exiting");
+				sensorsPerSegment++;
+			}
+		}
+		maxSensorsPerSegment = maxSensorsPerSegment>sensorsPerSegment ? maxSensorsPerSegment : sensorsPerSegment;
+	}
+	if(maxSensorsPerSegment > 1)
+		exitWithError("Bad parameters: having more than one sensor per segment is not supported. Exiting");
+}
 
-	/* ... */
-	return 0.;
+void ArrowbotSimulator::validateArrowbotSimulationParameters()
+{
+	if(simParameters.targetOrientations.size() == 0)
+		exitWithError("Bad simulation parameters: no target orientations supplied. Exiting");
+	if(simParameters.targetOrientations.size() != simParameters.initialConditions.size())
+		exitWithError("Bad simulation parameters: initial conditions must be supplied for every target orientation. Exiting");
+	if(simParameters.targetOrientations(0).size() != segments() ||
+	   simParameters.initialConditions(0).size() != segments())
+		exitWithError("Bad simulation parameters: size of target orientation or initial conditions vectors does not match the number of segments. Exiting");
+	if(simParameters.totalTime > simParameters.timeStep)
+		exitWithError("Bad simulation parameters: time step must be less than or equal to total simulation time. Exiting")
 }
 
 void ArrowbotSimulator::validateController()
@@ -33,8 +73,9 @@ void ArrowbotSimulator::validateController()
 	std::tie(sensors, motors) = currentController->shape();
 	if(sensors!=2*segments() || motors!=segments())
 	{
-		std::cerr << "ANN size (" << sensors << ", " << motors << ") does not match robot size (" << 2*segments() << ", " << segments() << ", exiting\n";
-		exit(EXIT_FAILURE);
+		std:ostringstream os;
+		os << "ANN size (" << sensors << ", " << motors << ") does not match robot size (" << 2*segments() << ", " << segments() << ", exiting";
+		exitWithError(os.str());
 	}
 }
 
@@ -53,17 +94,60 @@ void ArrowbotSimulator::parseController(matrix<double>& W, matrix<double>& Y)
 		}
 }
 
+double ArrowbotSimulator::evaluateControllerForOrientations(int orientationsIdx)
+{
+	typedef vector<double> stateType;
+
+	class ArrowbotRHS
+	{
+		const matrix<double>& m_phiCoefficient;
+		stateType psiContrib;
+
+		public:
+
+		ArrowbotRHS(const matrix<double>& phiCoeff, const matrix<double>& psiCoeff, const vector<double>& targetOrientations) :
+			m_phiCoefficient(phiCoeff)
+		{
+			psiContrib = prod(psiCoeff, targetOrientations);
+		};
+		void operator()(const stateType& x, stateType& dxdt, const double /* t */)
+		{
+			dxdt = psiContrib;
+			axpy_prod(m_phiCoefficient, x, dxdt, false);
+		};
+	};
+
+	class ArrowbotObserver
+	{
+		public:
+
+		void operator()(const stateType& x, double t)
+		{
+			std::cout << "t: " << t << " state: " << x << std::endl;
+		}
+	};
+
+	using namespace boost::numeric::odeint;
+
+	stateType currentState = simParameters.initialConditions(orientationsIdx);
+	runge_kutta4<stateType> stepper;
+	ArrowbotRHS abtRHS(phiCoefficient, psiCefficient, simParameters.targetOrientations(orientationsIdx));
+	ArrowbotObserver obs;
+	integrate_const(stepper, abtRHS, currentState, 0.0, simParameters.totalTime, simParameters.timeStep, obs);
+
+	return 0.;
+}
+
 // Public
 
 void ArrowbotSimulator::wire(ANNDirect* newController)
 {
 	currentController = newController;
-	validateCurrentController();
-	matrix<double> W,Y,K;
-	parseCurrentController(W, Y);
+	validateController();
+	matrix<double> Y, K;
+	parseCurrentController(psiCoefficient, Y);
 	lowerTriangularOnes(K, segments());
-	phiCoefficient = Y - prod(W, prod(parameters.sensorAttachment, K));
-	psiCoefficient = W;
+	phiCoefficient = Y - prod(psiCoefficient, prod(botParameters.sensorAttachment, K));
 }
 
 void ArrowbotSimulator::evaluateController()
--- arrowbotSimulator.h
+++ arrowbotSimulator.h
@@ -1,9 +1,6 @@
 #ifndef ARROWBOT_SIMULATOR_H
 #define ARROWBOT_SIMULATOR_H
 
-#include <vector>
-#include <tuple>
-
 #include <boost/numeric/ublas/matrix.hpp>
 
 #include "evclib/ann/direct.h"
@@ -12,36 +9,51 @@
    then run the simulation and evaluation computation with evaluateController().
    The simulation is implemented using GSL's ODE capabilities.
  */
-                                                                                
 
 using namespace boost::numeric::ublas;
 
-typedef struct ArrowbotSimulatorParameters
+typedef struct ArrowbotParameters
 {
 	int segments;
-	std::vector<std::vector<double>> targetOrientations;
 	matrix<double> sensorAttachment;
-} ArrowbotSimulatorParameters;
+} ArrowbotParameters;
+
+typedef struct ArrowbotSimulationParameters
+{
+	double totalTime;
+	double timeStep;
+	vector<vector<double>> targetOrientations;
+	vector<vector<double>> initialConditions;
+} ArrowbotSimulationParameters;
 
 class ArrowbotSimulator
 {
 	private:
 
-	const ArrowbotSimulatorParameters* const parameters;
+	const ArrowbotParameters* const botParameters;
+	const ArrowbotSimulationParameters* const simulationParameters;
 	ANNDirect* currentController;
 	matrix<double> phiCoefficient, psiCoefficient;
 
+	void validateArrowbotParameters();
+	void validateArrowbotSimulationParameters();
 	void validateController();
 	void parseController(matrix<double>& W, matrix<double>& Y);
-
 	double evaluateControllerForOrientations(int orientationsIdx);
 
 	public:
 
-	ArrowbotSimulator(const ArrowbotSimulatorParameters* p) : parameters(p), currentController(nullptr){};
+	ArrowbotSimulator(const ArrowbotParameters* p, const ArrowbotSimulationParameters* sp) :
+		parameters(p),
+		simulationParameters(sp),
+		currentController(nullptr)
+	{
+		validateArrowbotParameters();
+		validateArrowbotSimulationParameters();
+	};
 	void wire(ANNDirect* newController){currentController = newController;};
 	void evaluateController();
-	int segments(){return parameters.segments;} const;
+	inline int segments(){return botParameters.segments;} const;
 };
 
 #endif // ARROWBOT_SIMULATOR_H
